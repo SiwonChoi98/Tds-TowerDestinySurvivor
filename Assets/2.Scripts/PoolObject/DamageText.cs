a5c8b2b97f10f209e5f9d4d3eef2dc80b2270863
CHOI <qpqp615@gmail.com>
2025-03-10 23:33:12 +0900

    [fix] 데미지 텍스트 풀로 반환 수정

--- Assets/2.Scripts/PoolObject/DamageText.cs
+++ Assets/2.Scripts/PoolObject/DamageText.cs
@@ -7,9 +7,16 @@ using UnityEngine.UI;
 public class DamageText : BasePoolObject
 {
     [SerializeField] private Text _damage_T;
-    
+    private WaitForSeconds _waitForSeconds = new WaitForSeconds(0.5f);
     public void SetDamageText(float damage)
     {
         _damage_T.text = damage.ToString();
+        StartCoroutine(SetDamageTextCo());
+    }
+
+    private IEnumerator SetDamageTextCo()
+    {
+        yield return _waitForSeconds;
+        ReturnToPool();
     }
 }
